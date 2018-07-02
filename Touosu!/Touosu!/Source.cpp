ee6265f88f67bd47f9994c25b1707823104eb1b8
Евгений Зорин <zorin.eugene@gmail.com>
2018-07-02 16:35:23 +0600

    Revert "Test update"
    This reverts commit 8baaa54febc87c589df45ddefbf4de9127174392.

--- Touosu!/Touosu!/Source.cpp
+++ Touosu!/Touosu!/Source.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, Touhosu! I update!" << std::endl;
-	std::cout << "New string, hi" << std::endl;
+	std::cout << "Hello, Touhosu!" << std::endl;
 	std::system("pause");
 	return 0;
 }
